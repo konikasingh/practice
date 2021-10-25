f2e5403f6f53726f400c9d33b50ebd36fb8ea42a
konikasingh <konikasingh1996@gmail.com>
2021-10-25 12:15:00 +0530

    Welcome to Generics Examples

---
+++ vs/GenericsExample/GenericsExample/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace GenericsExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Generics World");
+        }
+    }
+}
